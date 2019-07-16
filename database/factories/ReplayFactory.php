bac66c9a53e9f5efcd1ed47a2037374455abb69d
Gerardo Belot <gerardo.belot@gmail.com>
2019-07-16 12:38:26 -0600

    Configurando ReplayFactory

---
+++ database/factories/ReplayFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function(){
+            return factory('App\Thread')->create()->id;
+        },
+        'body' => $faker->paragraph,
+    ];
+});
