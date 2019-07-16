12613c26f77deffcdd1f0238cc6435d6248653c0
Gerardo Belot <gerardo.belot@gmail.com>
2019-07-16 13:10:20 -0600

    <green> a thread see "/threads"

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadTest extends TestCase
 {
+    use RefreshDatabase;
+
+
     /** @test */
     public function a_user_can_browse_threads()
     {
