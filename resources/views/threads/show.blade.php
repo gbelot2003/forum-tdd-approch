f6b527d690611ab65a9188c684ff308e8d5fc766
Gerardo Belot <gerardo.belot@gmail.com>
2019-07-16 14:36:29 -0600

    <green> refactory Replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -18,7 +18,10 @@
                 @foreach($thread->replies as $reply)
                 <div class="card">
                     <div class="card-header">
-                        {{ $reply->created_at }}
+                        <a href="#">
+                            {{ $reply->owner->name }}
+                        </a>
+                        said {{ $reply->created_at->diffForHumans() }} ...
                     </div>
                     <div class="card-body">
                         {{ $reply->body }}
