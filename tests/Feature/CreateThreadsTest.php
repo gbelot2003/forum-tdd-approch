6eecd719ea6c7724e3c61b0d4c7e404ef6da70c5
Gerardo Belot <gerardo.belot@gmail.com>
2019-07-16 18:36:10 -0600

    <Red> CreateThreadsTest incompleto

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        
+    }
+}
